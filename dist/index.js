f70f15494fd5401f6722d81bbfd3918c5ed155a8
“Sid <sid@devopsdirective.com>
2025-09-03 14:11:41 -0400

    add new log

--- dist/index.js
+++ dist/index.js
@@ -27270,6 +27270,7 @@ async function run() {
         const ms = coreExports.getInput('milliseconds');
         // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
         coreExports.debug(`Waiting ${ms} milliseconds ...`);
+        coreExports.info('hello, this is another info level log!');
         // Log the current timestamp, wait, then log the new timestamp
         coreExports.debug(new Date().toTimeString());
         await wait(parseInt(ms, 10));
